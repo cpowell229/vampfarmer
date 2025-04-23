763aee600336f1af7c8abc2abd0ff323b2c23ebc
cpowell229 <156929427+cpowell229@users.noreply.github.com>
2025-04-23 18:03:30 -0400

    edits

--- vampfarmer/scripts/player.gd
+++ vampfarmer/scripts/player.gd
@@ -7,6 +7,7 @@ var collected_l1_coins = 0
 var beat_level1 = false
 var attacking = false
 var health = 150
+var alive = true
 var enemy_in_range = false
 var enemy_attack_cooldown = true
 
@@ -15,6 +16,17 @@ func player():
 
 func _physics_process(delta: float) -> void:
 	attack()
+	move_and_slide()
+	enemy_attack()
+	update_health()
+
+	
+	if health <= 0:
+		alive = false
+		health = 0
+		$AnimatedSprite2D.play("Death")
+		print("player died...")
+		
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -22,21 +34,18 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_left"):
+	if not attacking:
+		var direction := Input.get_axis("ui_left", "ui_right")
 		$AnimatedSprite2D.play("Walk")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if velocity.x < 0:
-		$AnimatedSprite2D.flip_h = true
-	elif velocity.x > 0:
-		$AnimatedSprite2D.flip_h = false
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.x < 0:
+			$AnimatedSprite2D.flip_h = true
+		elif velocity.x > 0:
+			$AnimatedSprite2D.flip_h = false
 
-	move_and_slide()
 
 func attack():
 	if Input.is_action_just_pressed("attack") and not attacking:
@@ -44,6 +53,12 @@ func attack():
 		Global.is_attacking = true
 		$AnimatedSprite2D.play("Attack")
 		$deal_attack.start()
+func enemy_attack():
+	if enemy_in_range and enemy_attack_cooldown == true:
+		health = health - 15
+		enemy_attack_cooldown = false
+		$Attack_Cooldown.start()
+		print("player -15 health")
 
 func _on_attack_cooldown_timeout() -> void:
 	enemy_attack_cooldown = true
@@ -59,7 +74,7 @@ func _on_heal_timeout() -> void:
 	if health < 150:
 		health = health + 20
 		if health > 150:
-			health = 100
+			health = 150
 	if health <= 0:
 		health = 0
 func update_health():
